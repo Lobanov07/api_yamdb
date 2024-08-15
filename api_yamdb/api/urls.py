c69d8f5963f9da7465aca61dfb80a064c0fc6e62
shulikin <alex@shulikin.com>
2024-08-15 13:15:49 +0400

    150824+urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,19 +1,52 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (UserViewSet,
-                    APISignup, APIGetToken)
-
-router = routers.DefaultRouter()
-
-router.register('users', UserViewSet)
-
+from api.views import (
+    APISignup,
+    APIGetToken,
+    CategoriesViewSet,
+    GenresViewSet,
+    TitleViewSet,
+    ReviewViewSet,
+    CommentViewSet,
+    UserViewSet,
+)
 
 auth = [
     path('signup/', APISignup.as_view(), name='register'),
     path('token/', APIGetToken.as_view(), name='token')
 ]
 
+router = routers.DefaultRouter()
+router.register(
+    'categories', 
+    CategoriesViewSet,
+    basename='categories'
+)
+router.register(
+    'genres',
+    GenresViewSet,
+    basename='genres'
+)
+router.register(
+    'titles',
+    TitleViewSet,
+    basename='titles'
+)
+router.register(
+    'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register(
+    'users',
+    UserViewSet,
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
